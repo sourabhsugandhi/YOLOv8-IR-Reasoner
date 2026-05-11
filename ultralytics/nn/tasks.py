f4d7b2f5f4e65ace3bc4f0a1fb8cfe32316522f3
UltralyticsAssistant <web@ultralytics.com>
2026-05-11 15:57:30 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -72,7 +72,6 @@ from ultralytics.nn.modules import (
     YOLOESegment,
     YOLOESegment26,
     v10Detect,
-    Reasoner,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, LOGGER, WINDOWS, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
